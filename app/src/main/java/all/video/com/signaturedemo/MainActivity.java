f3164e9345f57dd1526dcd6160f6448649f86b2d
yangbo <yangbo@nicaifu.com>
2017-10-10 11:54:46 +0800

    readme

--- app/src/main/java/all/video/com/signaturedemo/MainActivity.java
+++ app/src/main/java/all/video/com/signaturedemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     SignatureView signatureView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
